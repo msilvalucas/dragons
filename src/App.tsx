d81ef877cae0e45cc96803fa10cf22caa8dbbc05
Lucas Gomes <lucasgomess2109@gmail.com>
2025-04-20 18:13:26 -0300

    feat: add project fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { Button } from './components/ui/Button/Button';
+import { Input } from './components/ui/Input/Input';
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Input label="E-mail" type="email" />
+      <Button variant="primary">Primário</Button>
+      <Button variant="secondary">Secundário</Button>
+      <Button variant="danger">Perigo</Button>
+      <Button variant="warning">Aviso</Button>
+    </div>
+  );
 }
 
 export default App;
