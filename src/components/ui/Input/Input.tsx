1baece56ce3272d7d1c56e014dbdf43c3ac2ac88
Lucas Gomes <lucasgomess2109@gmail.com>
2025-04-20 23:29:47 -0300

    feat(UI): create input component

--- src/components/ui/Input/Input.tsx
+++ src/components/ui/Input/Input.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
-
+import styles from './Input.module.css';
 interface InputProps extends React.ComponentProps<'input'> {
   label: string;
 }
 
 export const Input = ({ label, ...props }: InputProps) => {
   return (
-    <div>
+    <div className={styles.container}>
       <label htmlFor={label}>{label}</label>
-      <input id={label} name={label} type="text" {...props} />
+      <input
+        id={label}
+        name={label}
+        type="text"
+        className={styles.input}
+        {...props}
+      />
     </div>
   );
 };
