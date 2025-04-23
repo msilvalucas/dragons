376a0c36222b744409c30b31c31b58ec6792e6b6
Lucas Gomes <lucasgomess2109@gmail.com>
2025-04-22 23:36:38 -0300

    feat: import pages

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,19 +4,28 @@ import { Home } from '@/pages/Home';
 import { SignIn } from '@/pages/SignIn';
 import { SignUp } from '@/pages/SignUp';
 
+import { NotFound } from '@/pages/NotFound';
+
 import { AuthGuard } from './AuthGuard';
+import RegisterDragon from '@/pages/RegisterDragon/RegisterDragon';
+import DetailsDragon from '@/pages/DatailsDragon/DetailsDragon';
 
 export function Router() {
   return (
     <Routes>
       <Route element={<AuthGuard isPrivate />}>
         <Route path="/" element={<Home />} />
+        <Route path="/dragoes/:id" element={<DetailsDragon />} />
+        <Route path="/dragoes/novo" element={<RegisterDragon />} />
+        <Route path="/dragoes/:id/editar" element={<RegisterDragon />} />
       </Route>
 
       <Route element={<AuthGuard isPrivate={false} />}>
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/cadastro" element={<SignUp />} />
       </Route>
+
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
