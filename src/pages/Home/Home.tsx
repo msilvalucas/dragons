9c5014e1b3736983a2ec55f961bdc44bedd9501f
Lucas Gomes <lucasgomess2109@gmail.com>
2025-04-22 23:37:33 -0300

    feat: add componente Header na home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,11 @@
 import { DragonList } from '@/components/dragon/DragonList';
 
+import { Header } from '@/components/ui/Header/Header';
+
 export const Home = () => {
   return (
     <>
+      <Header />
       <DragonList />
     </>
   );
