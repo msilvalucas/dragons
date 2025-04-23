67b8cfe94b5f32ef21cac48f4fbbd0adda79827e
Lucas Gomes <lucasgomess2109@gmail.com>
2025-04-22 23:21:45 -0300

    feat: add toastContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { BrowserRouter } from 'react-router';
 
-import { AuthProvider } from './context/auth/AuthContext';
+import { AuthProvider } from '@/context/auth/AuthContext';
+import { ToastProvider } from './context/toast/ToastContext';
 
 import { Router } from './routes';
 
 function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-    </AuthProvider>
+    <ToastProvider>
+      <AuthProvider>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </AuthProvider>
+    </ToastProvider>
   );
 }
 
